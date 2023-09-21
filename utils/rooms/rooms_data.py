907085e4bd09a815af4867d229870cfee64f79ef
Eduardo Olmos <olmos.edward@gmail.com>
2023-09-21 11:29:52 +0200

    initialize db with start data

--- utils/rooms/rooms_data.py
+++
@@ -1,7 +0,0 @@
-room_data = {
-        "rooms": [
-		{ "id": 1, "opening": "08:00", "closing": "20:00", "capacity": 4 },
-		{ "id": 2, "opening": "08:00", "closing": "14:00", "capacity": 10 },
-		{ "id": 3, "opening": "10:00", "closing": "18:00", "capacity": 6 }
-    ]
-    }
