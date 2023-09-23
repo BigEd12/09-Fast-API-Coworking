fec729e0d5c473e37319e38f4e2fc77d21975a68
Eduardo Olmos <olmos.edward@gmail.com>
2023-09-23 19:24:09 +0200

    add docstrings and check functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Depends, Form
+from fastapi import FastAPI, Depends, Form, Query
 from datetime import datetime
 from datetime import timedelta
+from pydantic import Field
 
 from database.models import Room, Client, Booking
 from database.db import Session
@@ -20,25 +21,35 @@ def get_db_session():
 
 @app.post('/data/load')
 def load_initial_data(session: Session = Depends(get_db_session)):
+    """
+    Populates database with initial data if database is empty
+    """
     try:
-        for room_data in data.get("rooms", []):
-            room_data["room_id"] = room_data.pop("id")
-            room_data["opening"] = room_data["opening"]
-            room_data["closing"] = room_data["closing"]
-            room = Room(**room_data)
-            session.add(room)
+        if (
+            session.query(Room).count() == 0 and
+            session.query(Client).count() == 0 and
+            session.query(Booking).count() == 0
+        ):
+            for room_data in data.get("rooms", []):
+                room_data["room_id"] = room_data.pop("id")
+                room_data["opening"] = room_data["opening"]
+                room_data["closing"] = room_data["closing"]
+                room = Room(**room_data)
+                session.add(room)
 
-        for client_data in data.get("clients", []):
-            client_data["client_id"] = client_data.pop("id")
-            client = Client(**client_data)
-            session.add(client)
+            for client_data in data.get("clients", []):
+                client_data["client_id"] = client_data.pop("id")
+                client = Client(**client_data)
+                session.add(client)
 
-        for booking_data in data.get("bookings", []):
-            booking = Booking(**booking_data)
-            session.add(booking)
+            for booking_data in data.get("bookings", []):
+                booking = Booking(**booking_data)
+                session.add(booking)
 
-        session.commit()
-        return {"message": "Data added to the database successfully."}
+            session.commit()
+            return {"message": "Data added to the database successfully."}
+        else:
+            return {'message': 'Database is already populated.'}
     except Exception as e:
         return {"error": str(e)}
 
@@ -46,51 +57,71 @@ def load_initial_data(session: Session = Depends(get_db_session)):
 
 @app.get('/bookings')
 def get_all_bookings(session: Session = Depends(get_db_session)):
+    """
+    Returns all bookings in database
+    """
     try:
         bookings = session.query(Booking).all()
-        booking_list = [{'id_room': booking.id_room, 'id_client': booking.id_client, 'start': booking.start, 'end': booking.end} for booking in bookings]
-        
-        return {"bookings": booking_list}
+        return {"All bookings": bookings}
     except Exception as e:
         return {"error": str(e)}
     
 @app.post('/bookings')
 def make_new_booking(
-    id_room: int = Form(...),
-    id_client: int = Form(...),
-    start: str = Form(...),
-    end: str = Form(...),
+    id_room: int = Query(..., description='Room Id'),
+    id_client: int = Query(..., description='Client Id'),
+    start: str = Query(..., description='Booking start time (YYYY-MM-DDTHH:MMZ - Where "T" and "Z" remain unchanged)'),
+    end: str = Query(..., description='Booking end time (YYYY-MM-DDTHH:MMZ - Where "T" and "Z" remain unchanged)'),
     session: Session = Depends(get_db_session)
     ):
+    """
+    Make a new booking with required parameters
+    """
 
     try:
-        new_booking = Booking(id_room=id_room, id_client=id_client, start=start, end=end)
+        new_booking = Booking(
+            id_room=id_room,
+            id_client=id_client,
+            start=start, end=end
+            )
+        
         session.add(new_booking)
         session.commit()
-        
         added_booking = session.query(Booking).filter(Booking.id == new_booking.id).first()
         
-        return {'message': 'new booking made successfully', 'new booking': added_booking }
+        return {'Booking confirmed': added_booking}
     except Exception as e:
         return {"error": str(e)}
 
-@app.get('/bookings/client/{client_id}')
-def get_bookings_by_client(client_id: int, session: Session = Depends(get_db_session)):
+@app.get('/bookings/client')
+def get_bookings_by_client(
+    client_id: int = Query(..., description='Client Id'),
+    session: Session = Depends(get_db_session)
+    ):
+    """
+    Get all bookings made by a queried client
+    """
     try:
         bookings = session.query(Booking).filter(Booking.id_client == client_id).all()
         booking_list = [{'id_room': booking.id_room, 'id_client': booking.id_client, 'start': booking.start, 'end': booking.end} for booking in bookings]
     
-        return {f'bookings by client {client_id}': booking_list}
+        return {f'Bookings by client {client_id}': booking_list}
     except Exception as e:
         return {'error': str(e)}
 
-@app.get('/bookings/room/{room_id}')
-def get_bookings_by_room(room_id: int, session: Session = Depends(get_db_session)):
+@app.get('/bookings/room')
+def get_bookings_by_room(
+    room_id: int = Query(..., description='Room Id'),
+    session: Session = Depends(get_db_session)
+    ):
+    """
+    Get all bookings for a queried room
+    """
     try:
         bookings = session.query(Booking).filter(Booking.id_room == room_id).all()
         booking_list = [{'id_room': booking.id_room, 'id_client': booking.id_client, 'start': booking.start, 'end': booking.end} for booking in bookings]
     
-        return {f'bookings for room {room_id}': booking_list}
+        return {f'Bookings for room {room_id}': booking_list}
     except Exception as e:
         return {'error': str(e)}
 
@@ -98,50 +129,61 @@ def get_bookings_by_room(room_id: int, session: Session = Depends(get_db_session
 
 @app.get('/rooms')
 def get_all_rooms(session: Session = Depends(get_db_session)):
+    """
+    Gets information on all rooms
+    """
     try:
         rooms = session.query(Room).all()
         room_list = [{"room_id": room.room_id, "opening": room.opening, "closing": room.closing, "capacity": room.capacity} for room in rooms]
         
-        return {"rooms": room_list}
+        return {"Rooms": room_list}
     except Exception as e:
         return {"error": str(e)}
     
 @app.post('/rooms')
 def add_new_room(
-    opening: str = Form(...),
-    closing: str = Form(...),
-    capacity: int = Form(...),
+    opening: str = Query(..., description='Opening time of new room (HH:MM)'),
+    closing: str = Query(..., description='Closing time of new room (HH:MM)'),
+    capacity: int = Query(..., description='Capacity of new room'),
     session: Session = Depends(get_db_session)
     ):
-
-    try:
-        opening_time = datetime.strptime(opening, "%H:%M").time()
-        closing_time = datetime.strptime(closing, "%H:%M").time()
-        
-        new_room = Room(opening=opening_time, closing=closing_time, capacity=capacity)
+    """
+    Add a new room with required query parameters
+    """
+    try:        
+        new_room = Room(opening=opening, closing=closing, capacity=capacity)
         session.add(new_room)
         session.commit()
         
         added_room = session.query(Room).filter(Room.room_id == new_room.room_id).first()
         
-        return {'message': 'new room added successfully', 'new room': added_room }
+        return {'Room added': added_room}
     except Exception as e:
         return {"error": str(e)}
 
 
 @app.get('/rooms/usage')
 def get_rooms_usage(session: Session = Depends(get_db_session)):
+    """
+    Returns the percentage each room has been used
+    """
     
     def room_open_hours():
+        """
+        Calculates how long each room is open in a day
+        """
         rooms = session.query(Room).all()
         rooms_open_hours = {}
         for room in rooms:
             open_time = datetime.strptime(room.opening, "%H:%M")
             close_time = datetime.strptime(room.closing, "%H:%M")
-            rooms_open_hours[room.room_id] = (close_time - open_time).total_seconds() / 3600  # Convert to hours
+            rooms_open_hours[room.room_id] = (close_time - open_time).total_seconds() / 3600
         return rooms_open_hours
     
     def total_days_in_range():
+        """
+        Calculates the number of days in the range of bookings
+        """
         bookings = session.query(Booking).all()
         date_list = []
         for booking in bookings:
@@ -150,6 +192,9 @@ def get_rooms_usage(session: Session = Depends(get_db_session)):
         return len(unique_dates)
     
     def total_bookings_per_room():
+        """
+        Calculates total time each room was used
+        """
         bookings = session.query(Booking).all()
         total_room_usage = {}
         for booking in bookings:
@@ -165,6 +210,9 @@ def get_rooms_usage(session: Session = Depends(get_db_session)):
         return total_room_usage
     
     def total_open_hours_per_room():
+        """
+        Calculates total time each room is open
+        """
         open_hours = {}
         rooms_open_hours = room_open_hours()
         num_unique_dates = total_days_in_range()
@@ -173,36 +221,41 @@ def get_rooms_usage(session: Session = Depends(get_db_session)):
         return open_hours
     
     def calculate_percentage_per_room():
+        """
+        Calculates time room was used against open as a percentage
+        """
         bookings_per_room = total_bookings_per_room()
         open_hours_per_room = total_open_hours_per_room()
         percentage_dict = {}
-        for room_id, timedelta1 in bookings_per_room.items():
-            if room_id in open_hours_per_room:
-                timedelta2 = open_hours_per_room[room_id]
-                if timedelta2.total_seconds() == 0:
+        
+        for room_id, timedelta1 in open_hours_per_room.items():
+            if room_id in bookings_per_room:
+                timedelta2 = bookings_per_room[room_id]
+                if timedelta1.total_seconds() == 0:
                     percentage = 0.0
                 else:
-                    percentage = (timedelta1.total_seconds() / timedelta2.total_seconds()) * 100
-                percentage_dict[room_id] = percentage
+                    percentage = round((timedelta2.total_seconds() / timedelta1.total_seconds()) * 100, 0)
+            else:
+                percentage = 0.0
+            
+            percentage_dict[room_id] = f'{percentage}%'
+        
         return percentage_dict
 
     result = calculate_percentage_per_room()
 
-    return {'message': result}
-    
-    
-        
-        
-
+    return {'Usage percentage by room': result}
     
 
-    
-    
-    
-
-
-@app.get('/rooms/availability/{room_id}/{timestamp}')
-def check_room_availability(room_id: int, timestamp: str, session: Session = Depends(get_db_session)):
+@app.get('/rooms/availability')
+def check_room_availability(
+    room_id: int = Query(..., description='Room Id'),
+    timestamp: str = Query(..., description='Time to check availability (YYYY-MM-DDTHH:MMZ - Where "T" and "Z" remain unchanged)'),
+    session: Session = Depends(get_db_session)
+    ):
+    """
+    Returns queried room availability at the queried timestamp
+    """
     bookings = session.query(Booking).filter(Booking.id_room == room_id).all()
     date_format = "%Y-%m-%d %H:%M"
     query = datetime.strptime(timestamp.split('T')[0] + ' ' + timestamp.split('T')[1][:-1], date_format)
@@ -214,13 +267,16 @@ def check_room_availability(room_id: int, timestamp: str, session: Session = Dep
         end_date_time = datetime.strptime(end.split('T')[0] + ' ' + end.split('T')[1][:-1], date_format)
         
         if start_date_time <= query <= end_date_time:
-            return {f'message': f'room {room_id} busy at the requested time({query})'}
+            return {f'Room {room_id}': f'Busy at requested time({query})'}
         
-    return {f'message': f'room {room_id} available at the requested time({query})'}
+    return {f'Room {room_id}': f'Available at requested time({query})'}
         
 
 @app.get('/rooms/bookings/overlapping')
 def get_overlapping_bookings(session: Session = Depends(get_db_session)):
+    """
+    Gets all overlapping bookings that exist
+    """
     bookings = session.query(Booking).all()
     
     def overlap(booking1, booking2):
@@ -256,12 +312,15 @@ def get_overlapping_bookings(session: Session = Depends(get_db_session)):
             }
             overlapping_list.append(overlapping_dict)
         
-        return {'message': 'Overlapping bookings found', 'overlapping_bookings': overlapping_list}
+        return {f'{len(overlapping_list)} Overlapping bookings found':  overlapping_list}
 
 # #----- CLIENT ENDPOINT -----#
 
 @app.get('/clients/bookings')
 def get_bookings_by_all_clients(session: Session = Depends(get_db_session)):
+    """
+    Gets all bookings made by all clients
+    """
     try:
         client_ids = []
         client_count = {}
@@ -274,7 +333,7 @@ def get_bookings_by_all_clients(session: Session = Depends(get_db_session)):
                 client_count[client_id] += 1
             else:
                 client_count[client_id] = 1
-        return {'bookings per client': client_count}
+        return {'Bookings per client': client_count}
     except Exception as e:
         print(f"Error: {str(e)}")
         return {"error": "Internal Server Error"}
