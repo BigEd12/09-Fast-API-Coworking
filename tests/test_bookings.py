347f64586aa3ecd0284440ecdee16589cd43b980
Eduardo Olmos <olmos.edward@gmail.com>
2023-09-24 20:00:13 +0200

    add tests for bookin endpoints

--- tests/test_bookings.py
+++ tests/test_bookings.py
@@ -1,36 +1,78 @@
-from fastapi import Depends
+# from fastapi.testclient import TestClient
+
+# from database.db import Session
+# from database.models import Booking
+
+# from main import app
+
+# client = TestClient(app)
+
+# def get_db_session():
+#     db_session = Session()
+#     try:
+#         yield db_session
+#     finally:
+#         db_session.close()
+
+# #----- GET /bookings -----#
+# def test_bookings_response_code():
+#     response = client.get('/api/bookings')
+#     assert response.status_code == 200
+
+# def test_booking_response_type():
+#     response = client.get('/api/bookings')
+#     response_data = response.json()
+#     assert isinstance(response_data, dict)
+    
+# def test_booking_content():
+#     with Session() as session:
+#         response = client.get('/api/bookings')
+#         response_data = response.json()
+        
+#         bookings = session.query(Booking).all()
+#         if bookings:
+#             assert len(response_data) != 0
+#         else:
+#             assert len(response_data) == 0
+            
+# #----- POST /bookings -----#
+# def test_bookings_post_response_code():
+#     response = client.post('/api/bookings')
+#     assert response.status_code == 200
+
+
 from fastapi.testclient import TestClient
-
-from database.db import Session
-from database.models import Booking
-
 from main import app
 
 client = TestClient(app)
 
-def get_db_session():
-    db_session = Session()
-    try:
-        yield db_session
-    finally:
-        db_session.close()
-
-def test_bookings_response_code():
-    response = client.get('/api/bookings')
-    assert response.status_code == 200
-
-def test_booking_response_type():
-    response = client.get('/api/bookings')
-    response_data = response.json()
-    assert isinstance(response_data, dict)
-    
-def test_booking_content():
-    with Session() as session:
+class TestBookingEndpoints:
+    def test_get_all_bookings(self):
         response = client.get('/api/bookings')
+        assert response.status_code == 200
         response_data = response.json()
-        
-        bookings = session.query(Booking).all()
-        if bookings:
-            assert len(response_data) != 0
-        else:
-            assert len(response_data) == 0
+        assert isinstance(response_data, dict)
+        assert "All bookings" in response_data
+
+    def test_make_new_booking(self):
+        data = {
+            "id_room": 1,
+            "id_client": 1,
+            "start": "2023-09-24T12:00Z",
+            "end": "2023-09-24T14:00Z",
+        }
+        response = client.post('/api/bookings/make', data=data)
+        assert response.status_code == 200
+        response_data = response.json()
+        assert isinstance(response_data, dict)
+        assert "Booking confirmed" in response_data
+
+    def test_get_bookings_by_filter(self):
+        params = {
+            "client_id": 1,
+            "room_id": 1,
+        }
+        response = client.get('/api/bookings/filter', params=params)
+        assert response.status_code == 200
+        response_data = response.json()
+        assert isinstance(response_data, list)
--- tests/test_clients.py
+++ tests/test_clients.py
@@ -1,27 +1,30 @@
+
 from fastapi.testclient import TestClient
 from main import app
-
 from database.db import Session
 from database.models import Client
 
 client = TestClient(app)
 
-def test_client_response_code():
-    response = client.get('/api/clients/bookings')
-    assert response.status_code == 200
+class TestClientEndpoints:
+    def test_client_response_code(self):
+        response = client.get('/api/clients/bookings')
+        assert response.status_code == 200
 
-def test_client_response_type():
-    response = client.get('/api/clients/bookings')
-    response_data = response.json()
-    assert isinstance(response_data, dict)
-
-def test_client_content():
-    with Session() as session:
+    def test_client_response_type(self):
         response = client.get('/api/clients/bookings')
         response_data = response.json()
-        
-        clients = session.query(Client).all()
-        if clients:
-            assert len(response_data) != 0
-        else:
-            assert len(response_data) == 0
+        assert isinstance(response_data, dict)
+
+    def test_client_content(self):
+        # Assuming you have created some sample clients in your test database
+        with Session() as session:
+            response = client.get('/api/clients/bookings')
+            response_data = response.json()
+            
+            clients = session.query(Client).all()
+            if clients:
+                assert len(response_data) != 0
+            else:
+                assert len(response_data) == 0
+
